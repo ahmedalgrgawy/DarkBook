5c7b8af4cd3ecefe3f2ab6307cb23a4372b0da7d
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-13 21:21:00 +0200

    Init & Desktop Navbar

---
+++ src/Components/MobileNavbar.tsx
@@ -0,0 +1,10 @@
+
+const MobileNavbar = () => {
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
+
+export default MobileNavbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/Components/theme-provider";
+import Navbar from "@/Components/Navbar";
+import SideBar from "@/Components/SideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +27,38 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
+
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      <SideBar />
+                    </div>
+                    <div className="lg:col-span-9">
+                      {children}
+                    </div>
+                  </div>
+                </div>
+
+              </main>
+
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
