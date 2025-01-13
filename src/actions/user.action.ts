3827904521c1edef0e20e035e5a05c13220e63b8
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-13 22:02:21 +0200

    Clerk User Into DB

---
+++ src/actions/user.action.ts
@@ -0,0 +1,36 @@
+"use server"
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+
+export const syncUser = async () => {
+    try {
+        const user = await currentUser()
+        const { userId } = await auth()
+
+        if (!userId || !user) return;
+
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                clerkId: userId,
+            },
+        });
+
+        if (existingUser) return existingUser;
+
+        const newUser = await prisma.user.create({
+            data: {
+                clerkId: userId,
+                name: `${user.firstName || ""} ${user.lastName || ""}`,
+                username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+                email: user.emailAddresses[0].emailAddress,
+                image: user.imageUrl,
+            }
+        })
+
+        return newUser;
+
+    } catch (error) {
+        console.log("Error in syncUser", error);
+    }
+}
