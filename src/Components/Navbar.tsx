7bedb30f93d3d99da61d87985148407c7231ddf9
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-13 21:23:59 +0200

    Mobile Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -14,7 +14,7 @@ async function Navbar() {
                     </div>
 
                     <DesktopNavbar />
-                    {/* <MobileNavbar /> */}
+                    <MobileNavbar />
                 </div>
             </div>
         </nav>
