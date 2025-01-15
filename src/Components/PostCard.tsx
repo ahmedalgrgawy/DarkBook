48477296920209e0d84873720fb93603f3965bca
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-15 22:20:46 +0200

    Like & Unlike Logic

--- src/Components/PostCard.tsx
+++ src/Components/PostCard.tsx
@@ -1,7 +1,49 @@
+"use client"
+import { getPosts, toggleLikes } from "@/actions/post.actions"
+import { useAuth } from "@clerk/nextjs"
+import { useState } from "react"
 
-const PostCard = ({ post }: {
-    post: any
+type Posts = NonNullable<Awaited<ReturnType<typeof getPosts>>>;
+type Post = Posts[number];
+
+const PostCard = ({ post, userId }: {
+    post: Post,
+    userId: string | null
 }) => {
+
+    const { user } = useAuth()
+    const [newComment, setNewComment] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [isLiking, setIsLiking] = useState(false)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const [hasLiked, setHasLiked] = useState(post.likes.some((like) => like.userId === userId))
+    const [optimizedLikes, setOptimizedLikes] = useState(post._count.likes) // ui update optimization
+
+    const handleLike = async () => {
+        if (isLiking) return;
+
+        try {
+            setIsLiking(true)
+            setHasLiked(prev => !prev)
+            setOptimizedLikes(hasLiked ? optimizedLikes - 1 : optimizedLikes + 1)
+            await toggleLikes(post.id)
+        } catch (error) {
+            setOptimizedLikes(post._count.likes)
+            setHasLiked(post.likes.some((like) => like.userId === userId))
+        } finally {
+            setIsLiking(false)
+        }
+
+    }
+
+    const handleAddComment = async () => {
+
+    }
+
+    const handleDeletePosts = async () => {
+
+    }
+
     return (
         <div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getPosts } from "@/actions/post.actions";
+import { getDbUserId } from "@/actions/user.actions";
 import CreatePost from "@/Components/CreatePost";
 import PostCard from "@/Components/PostCard";
 import SuggestedUsers from "@/Components/SuggestedUsers";
@@ -8,6 +9,7 @@ export default async function Home() {
 
   const user = await currentUser()
   const posts = await getPosts()
+  const dbUserId = await getDbUserId()
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
@@ -18,7 +20,7 @@ export default async function Home() {
           {
             posts?.map((post) => {
               return (
-                <PostCard key={post.id} post={post} />
+                <PostCard key={post.id} post={post} userId={dbUserId} />
               )
             })
           }
